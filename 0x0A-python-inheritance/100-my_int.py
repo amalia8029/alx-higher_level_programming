9c0c1fa9821aa04c1b557576435d0c20d970e20e
amalia8029 <amalshadraoui@gmail.com>
2023-10-09 19:24:35 -0700

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""defines class Square that inherits from Rectangle"""
+# 100-my_int.py
+"""Defines a class MyInt that inherits from int."""
 
 
-Rectangle = __import__('8-rectangle').Rectangle
+class MyInt(int):
+    """Invert int operators == and !=."""
 
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
-class Square(Rectangle):
-    """class for square that inherits from Rectangle
-    with method for area and string representation"""
-    def __init__(self, size):
-        """initializes Square instance"""
-        self.integer_validator("size", size)
-        self.__size = size
-
-    def area(self):
-        """returns area of square"""
-        return (self.__size * self.__size)
-
-    def __str__(self):
-        """string representation of Square"""
-        str_rep = "[Square] " + str(self.__size) + "/" + str(self.__size)
-        return str_rep
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
