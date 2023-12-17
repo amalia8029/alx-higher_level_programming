a920f98da6f16d3d64a5a4ca208e5380a7bcd7f8
amalia8029 <amalshadraoui@gmail.com>
2023-12-17 12:13:58 -0800

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-"""Py script to list all state.ids that match with state entered"""
+"""
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
+
 from sys import argv
-from sqlalchemy import create_engine, func
+from model_state import State, Base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1], argv[2], argv[3]))
+    """
+    Access to the database and get a state
+    from the database.
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    flag = 0
-    instr = func.binary(argv[4])
-    for instance in session.query(State)\
-                           .filter(State.name == instr).order_by(State.id):
-        print('{}'.format(instance.id))
-        flag = 1
-    if not flag:
+
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print('{0}'.format(state.id))
+    else:
         print("Not found")
-    session.close()
-    engine.dispose()
